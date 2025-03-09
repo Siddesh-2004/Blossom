f198a89f6c6bd0d814eb673ec31b79a9067e1bb8
Siddesh-2004 <siddeshpoojary2004@gmail.com>
2025-03-09 23:13:39 +0530

    added footer scrolling cards

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Features from "../FeaturesList.jsx";
-import Heading from '../Heading.jsx';
-
+import Heading from "../Heading.jsx";
+import ScrollingCards from "./ScrollingCards.jsx";
 import Card from "../Card.jsx";
 import Nav from "../Navbar.jsx";
 import Banner from "./Banner.jsx";
 import "./Home.css";
 import OccassionCard from "../OccassionCard.jsx";
-
+import Footer from "../Footer.jsx"
 function Home() {
   let img1 =
     "https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-mauve-surprise-385687-m.jpg";
@@ -25,9 +25,78 @@ function Home() {
     "https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-rosy-gardenia-177811-m.jpg";
   const img8 =
     "https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-regal-blossom-389731-m.jpg";
+    const customer1 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐⭐",
+      reviewDate: "05-03-2019",
+      reviewContent: "Absolutely stunning flowers! They arrived fresh and on time. Highly recommend!",
+      reviewerName: "Aarav Mehta"
+    };
+    const customer2 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐",
+      reviewDate: "12-07-2021",
+      reviewContent: "Good service but the flowers could have been arranged better. Still loved them!",
+      reviewerName: "Priya Sharma"
+    };
+    const customer3 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐⭐",
+      reviewDate: "30-01-2023",
+      reviewContent: "The bouquet was gorgeous, and the fragrance lasted for days. Will order again!",
+      reviewerName: "Rohan Verma"
+    };
+    const customer4 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐",
+      reviewDate: "18-06-2022",
+      reviewContent: "Beautiful flowers but slightly delayed delivery. Otherwise, great experience!",
+      reviewerName: "Sanya Kapoor"
+    };
+    
+    const customer5 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐⭐",
+      reviewDate: "08-09-2020",
+      reviewContent: "Perfect gift for my mom's birthday. She absolutely loved it!",
+      reviewerName: "Vikram Joshi"
+    };
+    
+    const customer6 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐⭐",
+      reviewDate: "14-02-2018",
+      reviewContent: "Ordered for Valentine's Day. My partner was delighted! Beautiful arrangement!",
+      reviewerName: "Megha Singh"
+    };
+    
+    const customer7 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐",
+      reviewDate: "22-11-2017",
+      reviewContent: "Lovely flowers, but a bit expensive. Still, the quality is worth it!",
+      reviewerName: "Rahul Tiwari"
+    };
+    
+    const customer8 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐⭐",
+      reviewDate: "01-05-2021",
+      reviewContent: "Excellent customer service and a beautiful arrangement. Will buy again!",
+      reviewerName: "Ananya Gupta"
+    };
+    
+    const customer9 = {
+      imgUrl: "https://res.cloudinary.com/interflora/f_auto,q_auto,t_prodth/products/p-the-prosperity-duet-302335-m.jpg",
+      reviewStars: "⭐⭐⭐⭐",
+      reviewDate: "10-08-2016",
+      reviewContent: "The flowers were vibrant and fresh! Just wished the delivery was quicker.",
+      reviewerName: "Kunal Sharma"
+    };
+    const reviews = [customer1, customer2, customer3, customer4, customer5, customer6, customer7, customer8, customer9];
+    
   return (
     <>
-     
       <Banner
         banners={{
           images: [
@@ -37,32 +106,27 @@ function Home() {
           ],
         }}
       />
-      {/* <p className="occassionTitle">Celebrate with flowers</p>
-      <p className="occasionSubtitle">May the fragarence of happiness elevate your occasion</p> */}
-      <Heading heading1="Celebrate with flowers" heading2="May the fragarence of happiness elevate your occasion"  />
-      <div className="occassionConatainer">
-            <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/birthday_d_split_illustration_830_1738154831704"/>
-            <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/anniversary_d_split_illustration_830_1738154869104"/>
-           <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/wedding_gifts_d_split_illustration_830_1738155423856"/>
-           <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/grand_gestures_d_split_illustration_830_1738230308822"/>
-           <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/gifts_for_men_d_split_illustration_830_1738154921657"/>
-           <OccassionCard 
-          occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/congratulations_d_split_illustration_830_1738155459964"/>
 
+      <Heading
+        heading1="Celebrate with flowers"
+        heading2="May the fragarence of happiness elevate your occasion"
+      />
+      <div className="occassionConatainer">
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/birthday_d_split_illustration_830_1738154831704" />
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/anniversary_d_split_illustration_830_1738154869104" />
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/wedding_gifts_d_split_illustration_830_1738155423856" />
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/grand_gestures_d_split_illustration_830_1738230308822" />
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/gifts_for_men_d_split_illustration_830_1738154921657" />
+        <OccassionCard occassionImageUrl="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt10prodlp/banners/congratulations_d_split_illustration_830_1738155459964" />
       </div>
-      {/* <p className="bestSellingTitle">Best Selling</p>
-      <p className="bestSellingSubtitle">Floral arrangements that gets the most love</p>  */}
-      
-        <Heading heading1="Best Selling" heading2="Floral arrangements that gets the most love"/>
-  
-<div className="bestSellingContainer ">
-     
-      <Card
+
+      <Heading
+        heading1="Best Selling"
+        heading2="Floral arrangements that gets the most love"
+      />
+
+      <div className="bestSellingContainer ">
+        <Card
           flowers={{
             src: img1,
             name: "SkyPink Roses",
@@ -98,25 +162,47 @@ function Home() {
             price: 10999,
           }}
         />
+      </div>
 
-</div>
-
-
-<div className="backgroundImageContainer">
-  <p className="sentimentTitle sentimentTitle1">SOFT SENTIMENTS ,</p>
-    <p className="sentimentTitle sentimentTitle2">WHISPERED THROUGH FLOWERS</p> 
-  <div className="sentimentButtons">
-    <button className="sentimentButton">SEND<br/> THANK YOU</button>
-    <button className="sentimentButton">SEND <br/>GET WELL SOON</button>
-    <button className="sentimentButton">SEND <br/>CONGRATS</button>
-  </div> 
-  <div className="sentimentButtons">
-    <button className="sentimentButton">FOR <br/>HOUSEWARMING</button>
-    <button className="sentimentButton">FOR <br/>BABYSHOWER</button>
-    <button className="sentimentButton">FOR <br/>THE HOST</button>
-  </div>
-</div>
-
+      <div className="backgroundImageContainer">
+        <p className="sentimentTitle sentimentTitle1">SOFT SENTIMENTS ,</p>
+        <p className="sentimentTitle sentimentTitle2">
+          WHISPERED THROUGH FLOWERS
+        </p>
+        <div className="sentimentButtons">
+          <button className="sentimentButton">
+            SEND
+            <br /> THANK YOU
+          </button>
+          <button className="sentimentButton">
+            SEND <br />
+            GET WELL SOON
+          </button>
+          <button className="sentimentButton">
+            SEND <br />
+            CONGRATS
+          </button>
+        </div>
+        <div className="sentimentButtons">
+          <button className="sentimentButton">
+            FOR <br />
+            HOUSEWARMING
+          </button>
+          <button className="sentimentButton">
+            FOR <br />
+            BABYSHOWER
+          </button>
+          <button className="sentimentButton">
+            FOR <br />
+            THE HOST
+          </button>
+        </div>
+      </div>
+      <div className="customerReviewContainer">
+      <Heading heading1="Customer Reviews"/>
+        <ScrollingCards Reviews={reviews}/>
+      </div>
+      <Footer/>
     </>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ function Nav() {
           <ul className="navTabList1">
             <li>Home</li>
             <li>About</li>
-            <li>Services</li>
+           
             <li>Contact</li>
             <li>Help</li>
           </ul>
